2b8ed166c71566a9397f656c1a96c644eacc8822
renatoquerales28-byte <renatoquerales28@gmail.com>
2026-01-23 05:18:20 -0400

    Fix: Convert tailwind config to JS to fix build warning

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        cyan: {
+          500: "#00E0FF",
+        },
+        fuchsia: {
+          500: "#FF00E5",
+        },
+      },
+      fontFamily: {
+        outfit: ["var(--font-outfit)", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};

06d65bdf021b94f30094867bfec1c0db65704445
renatoquerales28-byte <renatoquerales28@gmail.com>
2026-01-23 20:07:56 -0400

    chore: Remove diagnostic page and cleanup

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,6 @@ export { default } from "next-auth/middleware";
 export const config = {
     matcher: [
         "/admin",
-        "/admin/((?!login|diagnose).*)",
+        "/admin/((?!login).*)",
     ]
 };
